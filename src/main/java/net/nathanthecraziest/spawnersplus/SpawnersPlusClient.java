16bd447f740b9aff664caa7b1afba6d3d4e90465
Taylor Silenzio <taylor@silenz.io>
2026-05-26 12:04:37 -0500

    feat: populate creative tab with one mob_soul per MobEntity type

--- src/main/java/net/nathanthecraziest/spawnersplus/SpawnersPlusClient.java
+++ src/main/java/net/nathanthecraziest/spawnersplus/SpawnersPlusClient.java
@@ -3,10 +3,18 @@ package net.nathanthecraziest.spawnersplus;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.registry.Registries;
+import net.minecraft.world.World;
 import net.nathanthecraziest.spawnersplus.blocks.ModBlocks;
 import net.nathanthecraziest.spawnersplus.client.MobSoulColorProvider;
+import net.nathanthecraziest.spawnersplus.items.ModItemGroup;
 import net.nathanthecraziest.spawnersplus.items.ModItems;
+import net.nathanthecraziest.spawnersplus.items.souls.MobSoulItem;
 
 public class SpawnersPlusClient implements ClientModInitializer {
     @Override
@@ -14,5 +22,23 @@ public class SpawnersPlusClient implements ClientModInitializer {
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.INACTIVE_SPAWNER, RenderLayer.getCutoutMipped());
 
         ColorProviderRegistry.ITEM.register(new MobSoulColorProvider(), ModItems.MOB_SOUL);
+
+        ModItemGroup.clientDynamicPopulator = entries -> {
+            MobSoulItem mobSoul = (MobSoulItem) ModItems.MOB_SOUL;
+            World probeWorld = MinecraftClient.getInstance().world;
+            if (probeWorld == null) return;
+            for (EntityType<?> type : Registries.ENTITY_TYPE) {
+                if (ModItemGroup.LEGACY_SOUL_TYPES.contains(type)) continue;
+                try {
+                    Entity probe = type.create(probeWorld);
+                    if (!(probe instanceof MobEntity)) {
+                        if (probe != null) probe.discard();
+                        continue;
+                    }
+                    probe.discard();
+                    entries.add(MobSoulItem.stackFor(mobSoul, type));
+                } catch (Throwable ignored) {}
+            }
+        };
     }
 }
--- src/main/java/net/nathanthecraziest/spawnersplus/items/ModItemGroup.java
+++ src/main/java/net/nathanthecraziest/spawnersplus/items/ModItemGroup.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.enchantment.EnchantmentLevelEntry;
 import net.minecraft.enchantment.EnchantmentTarget;
+import net.minecraft.entity.EntityType;
 import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -14,9 +15,19 @@ import net.nathanthecraziest.spawnersplus.SpawnersPlus;
 import net.nathanthecraziest.spawnersplus.blocks.ModBlocks;
 
 import java.util.EnumSet;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.function.Consumer;
 
 public class ModItemGroup {
+
+    public static final Set<EntityType<?>> LEGACY_SOUL_TYPES = new HashSet<>(Set.of(
+            EntityType.ZOMBIE, EntityType.SKELETON, EntityType.SPIDER, EntityType.CAVE_SPIDER,
+            EntityType.BLAZE, EntityType.MAGMA_CUBE, EntityType.SILVERFISH, EntityType.STRAY,
+            EntityType.WITHER_SKELETON, EntityType.HUSK, EntityType.DROWNED, EntityType.CREEPER
+    ));
+
+    public static Consumer<ItemGroup.Entries> clientDynamicPopulator = null;
 
     private static final ItemGroup ITEM_GROUP = FabricItemGroup.builder()
             .icon(() -> new ItemStack(ModItems.SPAWNER_FRAGMENT))
@@ -35,6 +46,10 @@ public class ModItemGroup {
                 entries.add(ModItems.WITHER_SKELETON_SOUL);
                 entries.add(ModItems.HUSK_SOUL);
                 entries.add(ModItems.CREEPER_SOUL);
+
+                Consumer<ItemGroup.Entries> populator = clientDynamicPopulator;
+                if (populator != null) populator.accept(entries);
+
                 entries.add(EnchantedBookItem.forEnchantment(new EnchantmentLevelEntry(ModEnchantments.SOUL_STEALING, 1)));
             })
             .build();
